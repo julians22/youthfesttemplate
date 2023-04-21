01ebb48a66248b46f49acf090a45a3347aa22f02
julians22 <dabnerjulian@yahoo.com>
2023-04-21 21:25:02 +0700

    add body script

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -17,6 +17,28 @@
     @stack('after-styles')
 </head>
 <body>
+    @stack('body-script')
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId: '776691933723342',
+                xfbml: true,
+                version: 'v16.0'
+            });
+            FB.AppEvents.logPageView();
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
     @include('includes.partials.read-only')
     @include('includes.partials.logged-in-as')
     @include('includes.partials.announcements')
